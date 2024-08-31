6d524361f6e45cc37ed20dac45f9d930956bfe34
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2024-09-01 01:48:57 +0800

    validateAccountCreation

--- src/routes/accountRoutes.js
+++ src/routes/accountRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { createAccount, getAccounts, getBalance, deleteAccount } = require('../controllers/accountController');
 const { authMiddleware } = require('../middleware/authMiddleware');
+const { validateAccountCreation } = require('../middleware/accountMiddleware')
 const router = express.Router();
 
-router.post('/create', authMiddleware, createAccount);
+router.post('/create', authMiddleware, validateAccountCreation, createAccount);
 router.get('/', authMiddleware, getAccounts);
 router.get('/balance/:accountNumber', authMiddleware, getBalance);
 router.delete('/:accountNumber', authMiddleware, deleteAccount);
