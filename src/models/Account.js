c54702294ff8401cb305250f8d2f22394d2051f1
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2024-08-31 23:39:23 +0800

    Update Account Schema

--- src/models/Account.js
+++ src/models/Account.js
@@ -6,16 +6,50 @@ const AccountSchema = new mongoose.Schema({
         ref: 'User',
         required: true,
     },
-    balance: {
-        type: Number,
-        required: true,
-        default: 0,
-    },
     accountNumber: {
         type: String,
         required: true,
         unique: true,
     },
+    accountType: {
+        type: String,
+        enum: ['Savings', 'Checking', 'Business'],
+        required: true,
+    },
+    branch: {
+        type: String,
+        required: true,
+    },
+    balance: {
+        type: Number,
+        default: 0,
+    },
+    interestRate: {
+        type: Number,
+    },
+    currency: {
+        type: String,
+        default: 'USD',
+    },
+    status: {
+        type: String,
+        enum: ['Active', 'Dormant', 'Closed'],
+        default: 'Active',
+    },
+    dateOpened: {
+        type: Date,
+        default: Date.now,
+    },
+    dateClosed: {
+        type: Date,
+    },
+    overdraftLimit: {
+        type: Number,
+    },
+    minimumBalance: {
+        type: Number,
+        default: 0,
+    }
 });
 
 module.exports = mongoose.model('Account', AccountSchema);
