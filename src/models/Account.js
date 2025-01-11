0684db6e96fbe225bc485edd38cc959c91aae277
Mohd Khairi Mohd Yusoff <mkhairiyusoff.1985@gmail.com>
2025-01-11 18:14:50 +0800

    Update AccountSchema

--- src/models/Account.js
+++ src/models/Account.js
@@ -1,55 +1,55 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const AccountSchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
-    },
-    accountNumber: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    accountType: {
-        type: String,
-        enum: ['Savings', 'Checking', 'Business'],
-        required: true,
-    },
-    branch: {
-        type: String,
-        required: true,
-    },
-    balance: {
-        type: Number,
-        default: 0,
-    },
-    interestRate: {
-        type: Number,
-    },
-    currency: {
-        type: String,
-        default: 'USD',
-    },
-    status: {
-        type: String,
-        enum: ['Active', 'Dormant', 'Closed'],
-        default: 'Active',
-    },
-    dateOpened: {
-        type: Date,
-        default: Date.now,
-    },
-    dateClosed: {
-        type: Date,
-    },
-    overdraftLimit: {
-        type: Number,
-    },
-    minimumBalance: {
-        type: Number,
-        default: 0,
-    }
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  accountNumber: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  accountType: {
+    type: String,
+    enum: ["Savings", "Checking", "Business"],
+    required: false,
+  },
+  branch: {
+    type: String,
+    required: false,
+  },
+  balance: {
+    type: Number,
+    default: 0,
+  },
+  interestRate: {
+    type: Number,
+  },
+  currency: {
+    type: String,
+    default: "USD",
+  },
+  status: {
+    type: String,
+    enum: ["Active", "Dormant", "Closed"],
+    default: "Active",
+  },
+  dateOpened: {
+    type: Date,
+    default: Date.now,
+  },
+  dateClosed: {
+    type: Date,
+  },
+  overdraftLimit: {
+    type: Number,
+  },
+  minimumBalance: {
+    type: Number,
+    default: 0,
+  },
 });
 
-module.exports = mongoose.model('Account', AccountSchema);
+module.exports = mongoose.model("Account", AccountSchema);
