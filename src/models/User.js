39e252be5dfcd718da455e204d376e467bda2670
Mohd Khairi Mohd Yusoff <mkhairiyusoff.1985@gmail.com>
2025-01-29 15:18:25 +0800

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,60 +1,70 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     phoneNumber: {
-        type: String,
+      type: String,
+    },
+    identityNumber: { type: String, unique: true },
+    isVerified: { type: Boolean, default: false },
+    role: {
+      type: String,
+      enum: ["customer", "banker", "admin"],
+      default: "customer",
     },
     address: {
-        street: String,
-        city: String,
-        state: String,
-        postalCode: String,
+      street: String,
+      city: String,
+      state: String,
+      postalCode: String,
     },
     dateOfBirth: {
-        type: Date,
+      type: Date,
     },
     preferences: {
-        theme: {
-            type: String,
-            default: 'light'
-        },
-        language: {
-            type: String,
-            default: 'en'
-        },
-        notifications: {
-            type: Boolean,
-            default: true
-        }
+      theme: {
+        type: String,
+        default: "light",
+      },
+      language: {
+        type: String,
+        default: "en",
+      },
+      notifications: {
+        type: Boolean,
+        default: true,
+      },
     },
     refreshToken: {
-        type: String
-    }
-}, {
-    timestamps: true
-});
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-UserSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) {
-        next();
-    }
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
     next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
