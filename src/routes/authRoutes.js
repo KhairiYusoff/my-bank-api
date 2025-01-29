273a19ecf5fea2c0d1f7276b30adfae653916c1f
Mohd Khairi Mohd Yusoff <mkhairiyusoff.1985@gmail.com>
2025-01-29 16:49:32 +0800

    Update auth routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,12 +1,21 @@
-const express = require('express');
-const { register, login, refreshToken, logout, checkToken } = require('../controllers/authController');
-const { authMiddleware, validateRegistration } = require('../middleware/authMiddleware');
+const express = require("express");
+const {
+  registerCustomer,
+  login,
+  refreshToken,
+  logout,
+  checkToken,
+} = require("../controllers/authController");
+const {
+  authMiddleware,
+  validateRegistration,
+} = require("../middleware/authMiddleware");
 const router = express.Router();
 
-router.post('/register', validateRegistration, register);
-router.post('/login', login);
-router.post('/refresh-token', refreshToken);
-router.post('/logout', authMiddleware, logout);
-router.get('/check-token', authMiddleware, checkToken);
+router.post("/register-customer", validateRegistration, registerCustomer);
+router.post("/login", login);
+router.post("/refresh-token", refreshToken);
+router.post("/logout", authMiddleware, logout);
+router.get("/check-token", authMiddleware, checkToken);
 
 module.exports = router;
