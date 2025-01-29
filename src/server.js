b64fee1f851f250a93d08d1a954e28f3f9c4e2da
Mohd Khairi Mohd Yusoff <mkhairiyusoff.1985@gmail.com>
2025-01-29 21:10:45 +0800

    add the new routes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
+const adminRoutes = require("./routes/adminRoutes");
+const verificationRoutes = require("./routes/verificationRoutes");
 const accountRoutes = require("./routes/accountRoutes");
 const transactionRoutes = require("./routes/transactionRoutes");
 const userRoutes = require("./routes/userRoutes");
@@ -16,6 +18,8 @@ app.use(express.json({ extended: false }));
 
 // Define Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/verification", verificationRoutes);
 app.use("/api/accounts", accountRoutes);
 app.use("/api/transactions", transactionRoutes);
 app.use("/api/users", userRoutes);
