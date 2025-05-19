5cf850389a146e5f0dc8b0aca1c517245f4d9961
Mohd Khairi Mohd Yusoff <mkhairiyusoff.1985@gmail.com>
2025-05-19 17:40:53 +0800

    Update account routes - implement proper RBAC

--- src/routes/accountRoutes.js
+++ src/routes/accountRoutes.js
@@ -1,12 +1,62 @@
-const express = require('express');
-const { createAccount, getAccounts, getBalance, deleteAccount } = require('../controllers/accountController');
-const { authMiddleware } = require('../middleware/authMiddleware');
-const { validateAccountCreation } = require('../middleware/accountMiddleware')
+const express = require("express");
+const {
+  createAccount,
+  getAccounts,
+  getBalance,
+  deleteAccount,
+  getAllAccounts,
+  deposit,
+  withdraw,
+  airdrop,
+} = require("../controllers/accountController");
+const {
+  authMiddleware,
+  authorizeRoles,
+} = require("../middleware/authMiddleware");
+const { validateAccountCreation } = require("../middleware/accountMiddleware");
 const router = express.Router();
 
-router.post('/create', authMiddleware, validateAccountCreation, createAccount);
-router.get('/', authMiddleware, getAccounts);
-router.get('/balance/:accountNumber', authMiddleware, getBalance);
-router.delete('/:accountNumber', authMiddleware, deleteAccount);
+// Only banker can create account
+router.post(
+  "/create",
+  authMiddleware,
+  authorizeRoles("banker"),
+  validateAccountCreation,
+  createAccount
+);
+// Only banker can delete account
+router.delete(
+  "/:accountNumber",
+  authMiddleware,
+  authorizeRoles("banker"),
+  deleteAccount
+);
+// Only admin can view all accounts
+router.get("/all", authMiddleware, authorizeRoles("admin"), getAllAccounts);
+// Only customer can view their own accounts
+router.get("/", authMiddleware, authorizeRoles("customer"), getAccounts);
+// Only customer can view their own balance
+router.get(
+  "/balance/:accountNumber",
+  authMiddleware,
+  authorizeRoles("customer"),
+  getBalance
+);
+// Deposit: banker (any), customer (own)
+router.post(
+  "/deposit",
+  authMiddleware,
+  authorizeRoles("customer", "banker"),
+  deposit
+);
+// Withdraw: banker (any), customer (own)
+router.post(
+  "/withdraw",
+  authMiddleware,
+  authorizeRoles("customer", "banker"),
+  withdraw
+);
+// Airdrop: admin only
+router.post("/airdrop", authMiddleware, authorizeRoles("admin"), airdrop);
 
 module.exports = router;
