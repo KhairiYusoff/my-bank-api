e8af4b7f77250402b4c2ce9f9076cc84bc9236ca
Mohd Khairi Mohd Yusoff <mkhairiyusoff.1985@gmail.com>
2025-07-06 01:55:12 +0800

    feat(security): Invalidate JWT after password change/reset via passwordChangedAt

--- src/models/User.js
+++ src/models/User.js
@@ -129,6 +129,10 @@ const UserSchema = new mongoose.Schema(
     refreshToken: {
       type: String,
     },
+    passwordChangedAt: {
+      type: Date,
+      default: null,
+    },
     employerName: {
       type: String,
       required: function () {
@@ -244,6 +248,7 @@ UserSchema.pre("save", async function (next) {
   }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
+  this.passwordChangedAt = new Date();
   next();
 });
 
